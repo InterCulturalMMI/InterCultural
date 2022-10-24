f3920ca8d6d7665dff6866cc60112f92e65d5dbf
h3ude <hugo.mmi.pro@gmail.com>
2022-10-24 12:01:33 +0200

    Matinée 24/10
    Ajouts des images header & footer
    Début création header

---
+++ php/main.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <title> INTERCULTURAL </title>
+  <link rel="stylesheet" href="../css/accueil.css">
+  <script src="../js/accueil.js"></script>
+</head>
+
+<body>
+  <div class="containerBar">
+    <div class="fondBar">
+      <a href="#top">
+        <img href="../img/logo.png"></img>
+      </a>
+
+
+      <div class="containerBoutons">
+
+        <div class="bouton">
+          <a href="#top">
+            <p>
+              ACCUEIL
+            </p>
+          </a>
+        </div>
+
+        <div class="bouton">
+          <a href="#pays">
+            <p>
+              PAYS
+            </p>
+          </a>
+        </div>
+
+        <div class="bouton">
+          <a href="./reservation.php">
+            <p>
+              RESERVER
+            </p>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <?php 
+
+  ?>
+</body>
+</html>
