28c0ab03e28354417e656ef4679d5bca2296512c
h3ude <95217935+h3ude@users.noreply.github.com>
2022-10-24 23:36:56 +0200

    Update main.php

--- php/main.php
+++ php/main.php
@@ -9,7 +9,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
   <nav class="containerBar">
     <div class="fondBar">
       <div class="logo">
@@ -21,19 +20,21 @@
       <div class="containerBoutons">
 
         <div class="bouton">
-=======
-  <!--<nav class="containerBar">
-      <div class="fondBar">
-        <div class="logo">
->>>>>>> 204903b1c7ffdb8de07ee6d19798c4eae7f645b3
           <a href="#top">
-            <img src="../img/logo.png"></img>
+            <p>
+              ACCUEIL
+            </p>
           </a>
         </div>
 
-        <div class="containerBoutons">
+        <div class="bouton">
+          <a href="#pays">
+            <p>
+              PAYS
+            </p>
+          </a>
+        </div>
 
-<<<<<<< HEAD
         <div class="bouton">
           <a href="./reservation.php">
             <p id="reservation">
@@ -41,45 +42,8 @@
             </p>
           </a>
         </div>
-=======
-          <div class="bouton">
-            <a href="#top">
-              <p>
-                ACCUEIL
-              </p>
-            </a>
-          </div>
->>>>>>> 204903b1c7ffdb8de07ee6d19798c4eae7f645b3
 
-          <div class="bouton">
-            <a href="#pays">
-              <p>
-                PAYS
-              </p>
-            </a>
-          </div>
-
-          <div class="bouton">
-            <a href="./reservation.php">
-              <p id="reservation">
-                RESERVER
-              </p>
-            </a>
-          </div>
-
-        </div>
       </div>
-  </nav>-->
-  <div class="mosaique">
-    <div class="sponsors">
-      <img src="../img/logo.png" alt="Logo InterCultural"></img>
-      <img src="../img/departement.png" alt="Logo Département Haute-Loire"></img>
-      <img src="../img/region_aura.png" alt="Logo Région Auvergne-Rhônes-Alpes"></img>
-      <img src="../img/site_touriste.png" alt="Logo Tourisme Auvergnant"></img>
-      <img src="../img/logo_feader.png" alt="Logo Europe"></img>
-    </div>
-    <div class="contact">
-      <input id="bouton" type="button" value="Contactez-nous">
     </div>
 </nav>
 
