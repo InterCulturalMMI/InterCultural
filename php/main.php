068763cc413241339275fc3a1e58c9731cc1d1f6
h3ude <hugo.mmi.pro@gmail.com>
2022-10-25 01:48:28 +0200

    Correction bug

--- php/main.php
+++ php/main.php
@@ -48,6 +48,7 @@
 </nav>
 
 <div class="imgFond">
+
   <div class="edition">
 
     <p class="bold">
@@ -60,6 +61,101 @@
 
   </div>
 </div>
+
+<div class="containerCestQuoi">
+
+  <div class="cestQuoi">
+
+    <div class="photoCestQuoi">
+    </div>
+
+    <div class="cestQuoiParagraph">
+      <h2>QU'EST CE QUE C'EST ?</h2><br>
+      <p><strong>INTERCULTURAL</strong> est une exposition inédite. Tout les <strong>5 ans</strong> nous vous invitons à découvrir la culture de <strong>5 pays</strong> à travers des zones réparties <strong>dans la ville du Puy en Velay en France.</strong></p>
+
+      <div class="barre">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="cinqPays">
+  <p class="bold">
+      5 &nbsp;
+    </p>
+
+    <p class="fin">
+      PAYS - 
+    </p>
+
+    <p class="bold">
+     &nbsp; 5 &nbsp;
+    </p>
+
+    <p class="fin">
+      CULTURES
+    </p>
+</div>
+
+<div class="containerAlea">
+  <div class="alea">
+    <div class="textAlea">
+      <p>SI VOUS <strong>N'ARRIVEZ PAS</strong> À FAIRE <strong>VOTRE CHOIX</strong></p>
+    </div>
+
+    <div class="boutonAlea">
+      <p>CLIQUEZ ICI</p>
+      
+      <div class="iconAlea">
+        <img src="../img/aleatoire.png"></img>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="containerCestQuoi">
+
+  <div class="cestQuoi">
+
+    <div class="photoCestQuoi">
+    </div>
+
+    <div class="cestQuoiParagraph">
+      <h2>OÙ SE PASSE L'ÉVENEMENT ?</h2><br>
+      <p><strong>INTERCULTURAL</strong> se déroule <strong>dans la ville du Puy en Velay</strong> en Auvergne en France. Une ville connué pour sa fabrication de la <strong>dentelle</strong> du Puy, la culture de la <strong>lentille verte</strong> du Puy et la production de <strong>verveine</strong> du Velay. Elle est aussi connue pour être le départ de la <strong>Via Podiensis, un des quatre chemins de Compostelle français.</strong></p>
+
+      <div class="barre">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<footer>
+  <div class="containerFooter">
+
+    <div class="containerFooterLogos">
+      <img src="../img/logo.png"></img>
+      <img src="../img/departement.png"></img>
+      <img src="../img/region_aura.png"></img>
+      <img src="../img/site_touriste.png"></img>
+      <img src="../img/logo_feader.png"></img>
+    </div>
+
+    <div class="contact">
+      <p>NOUS CONTACTER</p>
+    </div>
+
+    <div class="containerFooterIcons">
+      <img src="../img/christ.png"></img>
+      <img src="../img/porte.png"></img>
+      <img src="../img/taj-mahal.png"></img>
+      <img src="../img/pyramide.png"></img>
+      <img src="../img/ruines.png"></img>
+    </div>
+  </div>
+</footer>
   <?php 
 
   ?>
