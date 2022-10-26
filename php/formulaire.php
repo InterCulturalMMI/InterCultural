e308a262cd293d211dc5cf0e279f74685d689bc7
Intercultural <interculturalmmi@gmail.com>
2022-10-26 11:38:34 +0200

    Uptade BDD

---
+++ php/formulaire.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="../css/formulaire.css">
+  <script src="script.js"></script>
+</head>
+<body>
+    <div class="global">
+      <div class="connexion">
+        <form>
+          <!--<label for="pseudo">Pseudonyme</label><br>-->
+          <input type="pseudo" id="pseudo" name="pseudo" placeholder="Pseudonyme.."><br>
+          <!-- <label for="lname">Mot de passe</label><br> -->
+          <input type="password" id="mdp" name="mdp" placeholder="Mot de passe.."><br><br>
+          <input type="submit" value="Connexion">
+        </form> 
+      </div>
+    </div>
+</body>
+</html>
